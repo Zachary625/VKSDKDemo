9d4ae386e7a083a1c873e7f639a3692dc9a54c43
Zachary <zachary625@outlook.com>
2017-11-13 20:46:42 +0800

    1
    supporter faulty

---
+++ Assets/Scripts/VKDemoSceneGUI.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class VKDemoSceneGUI : MonoBehaviour {
+
+	public Text LogText;
+	public GameObject LoggedOutGUI; // logged out gui
+	public GameObject LoggedInGUI; // logged in gui
+	// public GameObject SocialGUI; // social gui(friends etc.)
+
+	void Awake() 
+	{
+	}
+
+	// Use this for initialization
+	void Start () {
+		_ButtonCallbacks.Add ("ButtonLogin", OnLoginButtonClick);
+		_ButtonCallbacks.Add ("ButtonLogout", OnLogoutButtonClick);
+
+		PrepareButtons (LoggedOutGUI);
+		PrepareButtons (LoggedInGUI);
+
+		DisplayGUI (LoggedOutGUI);
+
+		_LogClear ();
+		_Log ("VKDemoSceneGUI.Start()");
+	}
+
+	private void PrepareButtons(GameObject root)
+	{
+		foreach (Button childButton in root.GetComponentsInChildren<Button>(true)) {
+			Button clickedButton = childButton;
+			childButton.onClick.AddListener(delegate
+				{
+					ButtonCallbackHub(clickedButton);
+				}
+			);
+		}
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void _LogInfo(string text)
+	{
+		_Log (string.Format("<color=lime>{0}</color>", text));
+	}
+
+	private void _LogError(string text)
+	{
+		_Log (string.Format("<color=red>{0}</color>", text));
+	}
+
+	private void _LogWarning(string text)
+	{
+		_Log (string.Format("<color=yellow>{0}</color>", text));
+	}
+
+	private void _LogClear()
+	{
+		LogText.text = string.Empty;
+	}
+
+	private void _Log(string text)
+	{
+		LogText.text += ("\n" + text);
+	}
+
+	private delegate void ButtonCallback();
+	private Dictionary<string, ButtonCallback> _ButtonCallbacks = new Dictionary<string, ButtonCallback> ();
+
+	private void ButtonCallbackHub(Button button)
+	{
+		string key = button.name;
+		if (_ButtonCallbacks.ContainsKey (key)) {
+			_ButtonCallbacks[key]();
+		} else {
+			_LogError ("Unknow button " + key);
+		}
+	}
+
+	private void OnLoginButtonClick()
+	{
+		DisplayGUI (LoggedInGUI);
+	}
+
+	private void OnLogoutButtonClick()
+	{
+		DisplayGUI(LoggedOutGUI);
+	}
+
+	private void DisplayGUI(GameObject go)
+	{
+		LoggedInGUI.SetActive (LoggedInGUI == go);
+		LoggedOutGUI.SetActive (LoggedOutGUI == go);
+	}
+
+
+}
---
+++ Assets/Scripts/VKInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class VKInfo {
+	public static int AppID = 6257405;
+	public static string SecureKey = "Mnq5qUxqKL9fAlMCiotT";
+	public static string ServiceToken = "9a09b88b9a09b88b9a09b88bd99a56c27699a099a09b88bc000ad86163e6492aa16354e";
+
+	public static string URLScheme_Name_iOS { get { return "vk" + AppID; } }
+	public static string URLScheme_Scheme_iOS { get { return "vk" + AppID; } }
+}
